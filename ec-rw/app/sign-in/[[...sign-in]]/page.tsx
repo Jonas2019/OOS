dc67d6e6fb419e50c65d3257624cd3634ee77e2b
Jonas_QD <richmondjonas@gmail.com>
2024-05-14 16:24:00 -0700

    sign-in

--- ec-rw/app/sign-in/[[...sign-in]]/page.tsx
+++ ec-rw/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,5 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return (
-    <div className="login-container">
-      <SignIn path="/sign-in" />;
-    </div>
-  );
+  return <SignIn path="/sign-in" />;
 }
--- ec-rw/app/sign-up/[[...sign-up]]/page.tsx
+++ ec-rw/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,5 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return (
-    <div className="login-container">
-      <SignUp path="/sign-up" />
-    </div>
-  );
+  return <SignUp path="/sign-up" />;
 }
